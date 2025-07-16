54d545b307a038cf215301ca745b236fce37cb0d
martinss27 <leoalmeidamartins@gmail.com>
2025-07-16 20:30:48 -0300

    triangle another way

--- triangle.py
+++ triangle.py
@@ -1,21 +1,11 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
 
-        row = len(triangle)
-        bottom = triangle[-1]
+        num_rows = len(triangle)
+        min_paths = triangle[num_rows-1].copy()
 
-        for linha_atual in range(len(triangle) - 2, -1, -1): #percorrendo as linhas de baixo p cima
-            for indice_coluna in range(len(triangle[linha_atual])): # percorrendo cada numero de cada linha
-                
-                linha_abaixo = triangle[linha_atual + 1]
+        for r in range(num_rows-2,-1,-1): #inicio do bottom-up
+            for c in range(r+1):
+                min_paths[c] = min(min_paths[c], min_paths[c + 1]) + triangle[r][c]
 
-                valor_reto = linha_abaixo[indice_coluna]
-                valor_diagonal = linha_abaixo[indice_coluna + 1]
-
-                menor_valor = min(valor_reto, valor_diagonal)
-
-                soma = triangle[linha_atual][indice_coluna] + menor_valor
-
-                triangle[linha_atual][indice_coluna] = soma
-
-        return triangle[0][0]
+        return min_paths[0]
