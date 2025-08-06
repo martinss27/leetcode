5286b420aad967fe8755433930421f8669fd121a
martinss27 <leoalmeidamartins@gmail.com>
2025-08-06 16:01:21 -0300

    intersection of two arrays o(1) using set

--- intersection-of-two-arrays.py
+++ intersection-of-two-arrays.py
@@ -4,11 +4,13 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         result = []
+        seen = set()
         nums2_set = set(nums2)
 
         for n in nums1:
-            if n in nums2_set and n not in result:
+            if n in nums2_set and n not in seen:
                 result.append(n)
+                seen.add(n)
         return result
     
-# o(n)
+# o(1)
