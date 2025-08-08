22e65fb2b0555b35d935843556068b7e05fed2e9
martinss27 <leoalmeidamartins@gmail.com>
2025-08-08 14:28:00 -0300

    pascal triangle 2, better performance

--- pascals-triangle-II.py
+++ pascals-triangle-II.py
@@ -1,18 +1,7 @@
-# https://leetcode.com/problems/pascals-triangle-ii/
-
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-
-        current_row = [1]
-
-        for row_index in range(1, rowIndex + 1):
-            previous_row = current_row
-            current_row = [1]
-
-            for indice in range(len(previous_row) - 1):
-                value = previous_row[indice] + previous_row[indice + 1]
-                current_row.append(value)
-                
-            current_row.append(1)
-
-        return current_row
+        row = [1]
+        for i in range(1, rowIndex + 1):
+            val = row[-1] * (rowIndex - i + 1) // i
+            row.append(val)
+        return row
