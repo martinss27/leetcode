f574d3040ba0cae9eb1940daab4ee6d41f29721a
martinss27 <leoalmeidamartins@gmail.com>
2025-08-14 20:02:41 -0300

    _

--- missing-number.py
+++ missing-number.py
@@ -3,12 +3,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
 
-        nums.sort()
+        n = len(nums)
 
-        for idx in range(len(nums)):
-            if nums[idx] == idx:
-                pass
-            elif nums[idx] != idx:
-                return idx
-            
-        return len(nums)
+        expected = n * (n + 1) // 2
+        array_sum = sum(nums)
+
+        return expected - array_sum
+    
+'''in case [3,0,1] is given, the expected sum is 6 (0+1+2+3) and the actual sum is 4 (3+0+1), so the missing number is 2.'''
