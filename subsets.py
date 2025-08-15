87ece5f3c25c3a0cf05ee250caa63088f8f200b5
martinss27 <leoalmeidamartins@gmail.com>
2025-08-15 18:04:15 -0300

    subsets without explanation

--- subsets.py
+++ subsets.py
@@ -5,14 +5,14 @@ class Solution:
         
         result = [] #onde vou guardar os subsets
 
-        def backtrack(index, path): #index: a partir de qual pos em nums vou olhar | path: subset que esta sendo construido
-            result.append(path[:]) #add subset atual ao resultado, path[:] cria uma copia do path atual
+        def backtrack(index, path):
+            result.append(path[:])
             
-            for i in range(index, len(nums)): # itera sobre os elementos restantes p/ formar novos subsets
-                path.append(nums[i]) # add elemento atual nums[i] ao subset atual
-                backtrack(i + 1, path) #chama backtrack com prox index e o subset ''atualizado''
-                path.pop() #desfaz ultima adicao (backtrack) para testar outras combinacoes
+            for i in range(index, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
 
         
-        backtrack(0, []) # iniciando o backtracking com index 0 e um subset vazio
+        backtrack(0, [])
         return result
