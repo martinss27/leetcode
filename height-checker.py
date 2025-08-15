c4c9324deb1f9356c12d6308fce87366e6d54bd7
martinss27 <leoalmeidamartins@gmail.com>
2025-08-15 12:38:56 -0300

    _

---
+++ height-checker.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/height-checker/
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+
+        expected = sorted(heights)
+        result = 0
+
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                result += 1
+            
+        return result
