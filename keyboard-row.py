53d6bdde9401fd6f67c29004762bb915955a51e6
martinss27 <leoalmeidamartins@gmail.com>
2025-08-19 10:19:43 -0300

    keyboard row using set()

--- keyboard-row.py
+++ keyboard-row.py
@@ -1,37 +1,18 @@
-# https://leetcode.com/problems/keyboard-row/
-
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
+        # using sets to represent each row O(1)
+        rows = [
+            set("qwertyuiop"),
+            set("asdfghjkl"),
+            set("zxcvbnm"),
+        ]
+
         resultado = []
-        
-        first_row = "qwertyuiop"
-        second_row = "asdfghjkl"
-        third_row = "zxcvbnm"
-
         for w in words:
-            word_lower = w.lower()
-
-            if not word_lower:
-                continue
-
-            primeira_letra = word_lower[0]
-
-            linha_candidata = None 
-            if primeira_letra in first_row:
-                linha_candidata = first_row
-            elif primeira_letra in second_row:
-                linha_candidata = second_row
-            elif primeira_letra in third_row:
-                linha_candidata = third_row
-
-            palavra_valida = True 
-            for letra in word_lower:
-                
-                if letra not in linha_candidata:
-                    palavra_valida = False
-                    break 
-
-            if palavra_valida:
+            word_lower = set(w.lower())
+            
+            # check if the word's letters are a subset of any row
+           
+            if any(word_lower.issubset(r) for r in rows):
                 resultado.append(w)
-
-        return resultado 
+        return resultado
