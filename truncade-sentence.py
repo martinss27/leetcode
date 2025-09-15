cd622820f1946bc07d9ec10dda7dd47bdc14682b
martinss27 <leoalmeidamartins@gmail.com>
2025-09-15 13:21:13 -0300

    truncate sentence one line solution

--- truncade-sentence.py
+++ truncade-sentence.py
@@ -1,8 +1,5 @@
 # https://leetcode.com/problems/truncate-sentence/
 
 class Solution:
-    def truncateSentence(self, s: str, k: int) -> str:
-
-        splited_sentence = s.split()
-        
-        return " ".join(splited_sentence[:k])
+    def truncateSentence(self, s: str, k: int) -> str:      
+        return " ".join(s.split()[:k])
