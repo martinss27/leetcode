cd1f81f7001de1d007996779b55783fe5baf8b3a
martinss27 <leoalmeidamartins@gmail.com>
2026-02-17 22:20:40 -0300

    TwoSum but O(n log n) - sorting + two pointers

--- golang/TwoSum.go
+++ golang/TwoSum.go
@@ -1,14 +1,36 @@
+import "sort"
+
 func twoSum(nums []int, target int) []int {
-    seen := make(map[int]int)
+    type pair struct {
+        value int
+        index int
+    }
+
+    pairs := make([]pair, len(nums))
 
     for i, v := range nums {
-        complement := target - v
+        pairs[i] = pair{value: v, index: i}
+    }
 
-        if index, ok := seen[complement]; ok {
-            return []int{index, i}
+    sort.Slice(pairs, func(i, j int) bool {
+        return pairs[i].value < pairs[j].value
+    })
+
+    left := 0
+    right := len(pairs) - 1
+
+    for left < right {
+        sum := pairs[left].value + pairs[right].value
+
+        if sum == target {
+            return []int{pairs[left].index, pairs[right].index}
         }
 
-        seen[v] = i
+        if sum < target {
+            left++
+        } else {
+            right--
+        }
     }
 
     return nil
